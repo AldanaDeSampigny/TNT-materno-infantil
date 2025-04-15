d55dfbf13ba9ba14b4660bd6d462eaa8165d6186
SosoMilk <sofiailin10@gmail.com>
2025-04-15 01:58:25 -0300

    pantalla principal con 3 iconos

--- MaternoInfantil/app/src/main/java/com/example/materno_infantil/MainActivity.kt
+++ MaternoInfantil/app/src/main/java/com/example/materno_infantil/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.materno_infantil
 
+import com.example.materno_infantil.actividades.Vacunacion
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import com.example.materno_infantil.actividades.ControlMedico
+import com.example.materno_infantil.actividades.Lactancia
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val iconVacunacion = findViewById<ImageView>(R.id.vacunacion)
+        val iconControl = findViewById<ImageView>(R.id.control)
+        val iconLactancia = findViewById<ImageView>(R.id.lactancia)
+
+        iconVacunacion.setOnClickListener{
+            val tocar = Intent(this, Vacunacion::class.java)
+            startActivity(tocar)
+        }
+
+        iconControl.setOnClickListener{
+            val tocar = Intent(this, ControlMedico::class.java)
+            startActivity(tocar)
+        }
+
+        iconLactancia.setOnClickListener{
+            val tocar = Intent(this, Lactancia::class.java)
+            startActivity(tocar)
         }
     }
 }
